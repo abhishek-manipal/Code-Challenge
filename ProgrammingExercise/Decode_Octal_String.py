02c39e371907d6d99feb7be70d94202f497ff708
AbhishekMishraWoolies <amishra5@woolworths.com.au>
2021-05-29 21:03:29 +1000

    Completed the code python

--- ProgrammingExercise/Decode_Octal_String.py
+++ ProgrammingExercise/Decode_Octal_String.py
@@ -8,8 +8,8 @@ def encode_function(string):
     converted_list = convert_list(binary)
     print("Last step convert to encoded string", converted_list)
 
-def reverse_slicing(s):
-    return s[::-1]
+def reverse_slicing(string):
+    return string[::-1]
 
 def convert_decimal(reversed_string):
     sum = 0
@@ -45,3 +45,13 @@ def convert_list(reversed_binary):
     print(newlist)
     return newlist
 encode_function("31646541")
+"""
+encode_function("1163114742311135167021343424004141432061264036716605455"+
+                 "35070012425145143366515462107042710455720106717127670062"+
+                 "71704657770433346073017047360217626325467150763006577133"+
+                 "54152655466766041402716542312670131505761476052650004524"+
+                 "21616177052165224543311447543654741617367042213645643631"+
+                 "33346575330621635642541636644326535501666004333326756424"+
+                 "47003252221104064117622317044717471253")
+
+"""
