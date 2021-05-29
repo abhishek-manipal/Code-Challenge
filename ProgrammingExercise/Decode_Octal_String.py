77b32ebe91a9fe1343343d8671a6c870eb8b34a1
AbhishekMishraWoolies <amishra5@woolworths.com.au>
2021-05-29 21:14:35 +1000

    Remove print and optimised code

--- ProgrammingExercise/Decode_Octal_String.py
+++ ProgrammingExercise/Decode_Octal_String.py
@@ -1,15 +1,12 @@
 import math
 
 def encode_function(string):
-    reversed_string = reverse_slicing(string)
+    print(len(string))
+    reversed_string = string[::-1]
     sum = convert_decimal(reversed_string)
     binary = convert_binary(sum)
     reversed_binary = binary.reverse()
     converted_list = convert_list(binary)
-    print("Last step convert to encoded string", converted_list)
-
-def reverse_slicing(string):
-    return string[::-1]
 
 def convert_decimal(reversed_string):
     sum = 0
@@ -17,7 +14,6 @@ def convert_decimal(reversed_string):
     for i in reversed_string:
         sum = sum + math.pow(8, power)*int(i)
         power = power+1
-    print ("first step convert to decimal no is", math.trunc(sum))
     return(math.trunc(sum))
 
 def convert_binary(sum):
@@ -26,7 +22,6 @@ def convert_binary(sum):
         rem = int(sum%2)
         binary.append(rem)
         sum = int(sum/2)
-    print("Second step convert to binary digit is", binary)
     return(binary)
 
 def convert_list(reversed_binary):
