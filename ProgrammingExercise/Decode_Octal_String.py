c9f0d51f1bf33477a1b4e1b0d88497fb324439c0
AbhishekMishraWoolies <amishra5@woolworths.com.au>
2021-05-29 17:54:12 +1000

    Adding encode function

--- ProgrammingExercise/Decode_Octal_String.py
+++ ProgrammingExercise/Decode_Octal_String.py
@@ -7,11 +7,7 @@ def encode_function(string):
     binary = convert_binary(sum)
     reversed_binary = binary.reverse()
     print("Third step convert to original binary by reversing", binary)
-    i = len(reversed_binary)
-    while i>0:
-        substr = reversed_binary[i, i-8]
-        print(substr)
-        i = i-8
+    converted_list = convert_list(binary)
 
 def reverse_slicing(s):
     return s[::-1]
@@ -34,4 +30,22 @@ def convert_binary(sum):
     print("Second step convert to binary digit is", binary)
     return(binary)
 
+def convert_list(reversed_binary):
+    sum = 0
+    ctr = 0
+    newlist = []
+    for i in range(len(reversed_binary)-1, -1, -1):
+        print("value of i",i)
+        print("value of ctr",ctr)
+        print("value of sum", sum)
+        sum = sum + math.pow(2, ctr)*int(reversed_binary[i])
+        ctr = ctr+1
+        if ctr%8==0:
+            print("octal value reached",reversed_binary[i] )
+            newlist.append(math.trunc(sum))
+            print("reset the value of sum", sum)
+            sum = 0
+            ctr = ctr-8
+    print(newlist)
+    return newlist
 encode_function("31646541")
