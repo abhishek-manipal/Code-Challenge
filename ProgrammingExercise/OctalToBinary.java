601582b714c8f1841ad3f14919006a5f56888536
AbhishekMishraWoolies <amishra5@woolworths.com.au>
2021-05-30 13:04:52 +1000

    Adding conversion of octal to binary

--- ProgrammingExercise/OctalToBinary.java
+++ ProgrammingExercise/OctalToBinary.java
@@ -13,16 +13,64 @@
 
 public class OctalToBinary{
   public static void main(String []args) {
-    //System.out.println("Hello to the world of Abhishek");
     // Declare Test one string
     String str1 = "31646541";
-    // Declare Test one string
+    // Declare Test two string
     String str2 = "1163114742311135167021343424004141432061264036716605455350700124251451433665154621070427104557201067171276700627170465777043334607301704736021762632546715076300657713354152655466766041402716542312670131505761476052650004524216161770521652245433114475436547416173670422136456436313334657533062163564254163664432653550166600433332675642447003252221104064117622317044717471253";
+    ConvertOctalToBinary(str1);
     ConvertOctalToBinary(str1);
   }
 
   public static void ConvertOctalToBinary(String str) {
     System.out.println("Original string "+str);
+    int octal = Integer.parseInt(str);
+    System.out.println(octal);
+    int rev = 0;
+    int chk = 0;
+    int rem = 0;
+    String binary = "";
+    while (octal!=0){
+      rem = octal%10;
+      if (rem>7) {
+        chk = 1;
+        break;
+      }
+      rev = rem + (rev*10);
+      octal = octal/10;
+    }
+    if (chk == 0){
+      octal = rev;
+      while (octal!=0) {
+        rem = octal%10;
+        if (rem==0) {
+          binary = binary + "000";
+        } else if (rem==1) {
+          binary = binary + "001";
+        } else if (rem==2) {
+          binary = binary + "010";
+        } else if (rem==3) {
+          binary = binary + "011";
+        } else if (rem==4) {
+          binary = binary + "100";
+        } else if (rem==5) {
+          binary = binary + "101";
+        } else if (rem==6) {
+          binary = binary + "110";
+        } else if (rem==7) {
+          binary = binary + "111";
+        }
+        octal = octal/10;
+      }
+    } else {
+      System.out.println("Invalid value ");
+    }
+  }
+
+  public static void ConvertToList(String str) {
+    int sum = 0;
+    int ctr = 0;
+
 
   }
+
 }
