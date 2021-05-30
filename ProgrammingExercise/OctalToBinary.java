e088e5820e80ac441e22f401ae51a1b92998db87
AbhishekMishraWoolies <amishra5@woolworths.com.au>
2021-05-30 13:43:29 +1000

    Remove comments

--- ProgrammingExercise/OctalToBinary.java
+++ ProgrammingExercise/OctalToBinary.java
@@ -1,14 +1,4 @@
-//Problem:
-// Write a function to decode an octal string to a number in an array of
-// bytes. For example, "31646541" should produce [103, 77, 97].
-// The solution should work for long input. Try:
-// "1163114742311135167021343424004141432061264036716605455
-// 35070012425145143366515462107042710455720106717127670062
-// 71704657770433346073017047360217626325467150763006577133
-// 54152655466766041402716542312670131505761476052650004524
-// 21616177052165224543311447543654741617367042213645643631
-// 33346575330621635642541636644326535501666004333326756424
-// 47003252221104064117622317044717471253"
+// Author @Abhishek Mishra
 
 import java.util.*;
 import java.lang.Math;
@@ -25,12 +15,14 @@ public class OctalToBinary {
 
   public static void ConvertOctalToBinary(String str) {
     System.out.println("Original string "+str);
-    int octal = Integer.parseInt(str);
+    Integer octal = Integer.parseInt(str);
     System.out.println(octal);
-    int rev = 0;
-    int chk = 0;
-    int rem = 0;
+    Integer rev = 0;
+    Integer chk = 0;
+    Integer rem = 0;
     String binary = "";
+    List<String> ArrayOfList = new List<String>();
+
     while (octal!=0){
       rem = octal%10;
       if (rem>7) {
@@ -62,6 +54,8 @@ public class OctalToBinary {
           binary = binary + "111";
         }
         octal = octal/10;
+        ArrayOfList = ConvertToList(binary);
+        //ArrayOfList.reverse()
       }
     } else {
       System.out.println("Invalid value ");
@@ -69,17 +63,27 @@ public class OctalToBinary {
   }
 
   public List<String> ConvertToList(String binary) {
-    int sum = 0;
-    int ctr = 0;
-    int length = binary.length()-1;
+    Integer sum = 0;
+    Integer ctr = 0;
+    Integer i;
+    Integer length = binary.length()-1;
     List<String> list = new ArrayList<String>();
 
-    for (int i=length; i>-i; i = i-1){
-      //sum = sum + Math.pow(2, ctr)*Integer.parseInt(binary[i]);
+    for (i=length; i>-i; i = i-1){
+      char x = binary.charAt(i);
+      System.out.print(x);
+      //sum = sum + Math.pow(2, ctr)*Integer.parseInt(x);
     }
-
+    ctr = ctr+1;
+    if (ctr%8==0){
+      sum = 0;
+      ctr = ctr-8;
+      list.add(sum.toString());
+    } else if (i==0){
+      list.add(sum.toString());
+    }
+    sum = 0;
+    ctr = ctr-8;
     return list;
-
   }
-
 }
