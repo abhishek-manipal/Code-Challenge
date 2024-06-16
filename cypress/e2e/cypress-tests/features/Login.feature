ef3098f355e2e9da5cb5beb8e0c103f0d8bb15b4
Abhishek Mishra <abhishek.mishra@ixup.com>
2024-06-16 18:45:56 +1000

    Adding the creset password flow

--- cypress/e2e/cypress-tests/features/Login.feature
+++ cypress/e2e/cypress-tests/features/Login.feature
@@ -3,5 +3,5 @@ Feature:  Test the Login functionality
     Scenario: Valid login functionality 
     Given I go to the return-it app
     When I clear the username and password forms
-    And 
+    And I login using registered credentials
     Then I am logged into successful to dashboard
---
+++ cypress/e2e/cypress-tests/features/cypress/ResetPasswordSteps.js
@@ -0,0 +1,50 @@
+import { And, Given, Then, When } from '@badeball/cypress-cucumber-preprocessor';
+
+
+Given('I go to the return-it app', () => {
+    cy.visit('https://membersvic.returnit.com.au/')
+    cy.contains('Welcome').should('exist')
+    cy.contains('Login to CDS Vic East to continue.').should('exist')
+});
+
+When('I click on forgot password button', () => {
+    cy.get('form').contains('Forgot password?').click()
+
+});
+
+And('provide valid email credentials', () => {
+    cy.get("#email").clear()
+    cy.get("#email").type("abhishek@me.com")
+    cy.get('form').contains('Continue').click()
+});
+
+And('provide ivalid email credentials', () => {
+    cy.get("#email").clear()
+    cy.get("#email").type("InvalidEmailFormat")
+    cy.get('form').contains('Continue').click()
+
+});
+
+Then('the reset email is sent succesfully ', () => {
+    cy.contains('Check Your Email').should('exist')
+    cy.contains('Resend email').should('exist')
+
+});
+
+Then('the Error message is shown to user', () => {
+    cy.contains('Email is not valid').should('exist')
+    cy.contains('Back to Return-It CDS Vic East').should('exist')
+
+});
+
+    
+
+
+    
+
+   
+    
+    
+  
+  
+  
